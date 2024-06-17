f5ddcd0bd1d11bc2413a70a6d9134e68ee14ca37
shoevakramchik@gmail.com <shoevakramchik@gmail.com>
2024-06-18 01:47:18 +0300

    Добавьте файлы проекта.

---
+++ Practice-5-ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_5_ConsoleApp1
+{
+    internal class Program
+    {
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+        static void Print(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите длинное предложение");
+            string text = Console.ReadLine();
+            string[] words = SplitText(text);
+            Print(words);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice-5.1-ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_5._1_ConsoleApp1
+{
+    internal class Program
+    {
+        
+        static string SplitText(string text)
+        {
+            string[] word = text.Split(' ');
+            string resultText = ReverseWord(word);
+            return resultText;
+        }
+        static string ReverseWord(string[] text)
+        {
+            Array.Reverse(text);
+            string reverseText = string.Join(" ", text);
+            return reverseText;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напиши ряд слов");
+            string mytext = Console.ReadLine();
+            mytext = SplitText(mytext);
+            Console.WriteLine(mytext);
+            Console.ReadLine(); 
+        }
+    }
+
+}
+   
